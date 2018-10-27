47f4eb4d6a8050f03ce187dc2d43eaedd6f14c0c
BlueBlueSkyZZ <493813966@qq.com>
2018-10-27 22:28:22 +0800

    add uncompleted (control) code

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,22 +1,87 @@
 //index.js
+//见https://open.iot.10086.cn/doc/art665.html
 //获取应用实例
 const app = getApp()
 const apikey = "eNIzhrJ8mW0Pol4phyvUZj6EKRA="
+const deviceid = '501311474'
 var deviceConnected = "false"
 
-const deviceInfo = "https://api.heclouds.com/devices/501311474"
-const getDataStream = "https://api.heclouds.com/devices/501311474/datastreams"
+const deviceInfoURL = "https://api.heclouds.com/devices/" + deviceid
+const getDataStreamURL = "https://api.heclouds.com/devices/" + deviceid + "/datastreams"
+const sendCommandURL = "https://api.heclouds.com/cmds"
 
 function deviceInit() {
+  //初始化各个硬件的状态
+  wx.request({
+    url: getDataStreamURL,
+    header: {
+      'content-type': 'application/x-www-form-urlencoded',
+      "api-key": apikey
+    },
+    data: {
+      
+    },
+    success(res) {
+      for(var i=0; i<res.data.data.length; i++) {
+        var info = res.data.data[i]
+        
+        switch(info.id) {
+          case "Blue_Led":
+            if(info.current_value == 1) {
+              this.setData({ blue_checked:'true'})
+            } else {
+              this.setData({ blue_checked: 'false' })
+            }
+            break
+          case "Beep":
+          //TODO
+            break
+          case "Red_Led":
+            break
+          case "Tempreture":
+            break
+          case "Yellow_Led":
+            break
+          case "Green_Led":
+            break
+          case "Humidity":
+            break
+        }
+      }
+    }
+  })
+}
 
+function controlLED(hardware_id ,switch_value) {
+  //按钮发送命令控制硬件
+  wx.request({
+    url: sendCommandURL + "?device_id=" + deviceid,
+    method: 'POST',
+    header: {
+      'content-type': 'application/x-www-form-urlencoded',
+      "api-key": apikey
+    },
+    data: {
+      //TODO 设计自定义语言 blueled:{V}, 预感这边可能会有问题
+      hardware_id: switch_value
+    },
+    success(res) {
+      console.log("控制成功")
+      console.log(res)
+      if(1) {
+        alert("控制成功")
+      } else {
+        alert("设备不在线")
+      }
+    }
+  })
 }
 
 Page({
   onLoad: function () {
-    //加载时完成
-    
+    //加载时完成1.检查设备是否连接2.连接成功将数据显示在界面
     wx.request({
-      url: deviceInfo,
+      url: deviceInfoURL,
       header: {
         'content-type': 'application/x-www-form-urlencoded',
         "api-key": apikey
@@ -28,7 +93,7 @@ Page({
         console.log(res)
         console.log(res.data.data.online)
         if (res.data.data.online) {
-          console.log("false怎么执行？？")
+          deviceInit()//初始化按钮
           deviceConnected = "true"
         } else {
           console.log("false执行")
@@ -49,26 +114,13 @@ Page({
     }
   },
   change: function (event) {
-    console.log(event.detail.value)
     switch(event.currentTarget.id) {
       case 'blue_led':
-        wx.request({
-          url: getDataStream,
-          header: {
-            'content-type': 'application/x-www-form-urlencoded',
-            "api-key": apikey
-          },
-          data : {
-            
-          },
-          success(res) {
-            console.log('请求成功')
-            console.log(res)
-          }
-        })
         console.log('蓝')
+        controlLED('blueled', event.detail.value)
         break;
       case 'yellow_led':
+      //TODO
         console.log('黄')
         break;
       case 'green_led':
